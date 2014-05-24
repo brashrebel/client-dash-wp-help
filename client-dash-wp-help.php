12f26202586a3e53019401205d3f5865a9789bd6
Kyle <kyle@realbigmarketing.com>
2014-05-23 21:42:59 -0400

    Houston we have a loop
    Data has now been grabbed and rendered in a foreach. That was tricky.

--- client-dash-wp-help.php
+++ client-dash-wp-help.php
@@ -8,11 +8,11 @@ Author URI: http://realbigmarketing.com/staff/kyle
 */
 function cdwph_test() {
 $result = wp_remote_get( add_query_arg( 'time', time(), 'http://realbigsites.com/?wp-help-key=72f514d73c4ded27362f2f1983e1347d' ) );
-$body = wp_remote_retrieve_body( $result );
-$body = json_decode($body);
-foreach ( $body as $post ) {
-	return $post->post_title;
-}
+//$body = wp_remote_retrieve_body( $result );
+$posts = json_decode( $result['body'] );
+	foreach ($posts as $value) {
+		echo $value->post_title;
+	}
 }
 
 add_shortcode('cdwph', 'cdwph_test');
