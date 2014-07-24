5e6eb841bc5e5e2787079640113c60aad9ae12ac
brashrebel <kyle@realbigmarketing.com>
2014-07-23 21:39:51 -0400

    Version 1.4 compatible
    Converted to boilerplate code structure.
    Also repaired an issue with the stylesheet conditional enqueueing.
    Also added style.css.

--- client-dash-wp-help.php
+++ client-dash-wp-help.php
@@ -7,67 +7,114 @@ Author: Kyle Maurer
 Author URI: http://realbigmarketing.com/staff/kyle
 */
 
-// Notices for if CD is not active
-function cdwph_notices() {
-	if (!is_plugin_active( 'client-dash/client-dash.php' )) {
-	echo '<div class="error">Client Dash WP Help Add-on requires <b>Client Dash</b>. Please install <b>Client Dash</b> to continue using.</div>';
+class CDWPHelp {
+
+	/*
+	* These variables you can change
+	*/
+	// Define the plugin name
+	private $plugin = 'Client Dash WP Help Addon';
+	// Setup your prefix
+	private $pre = 'cdwph';
+	// Set this to be name of your content block
+	private $block_name = 'WP Help';
+	// Set the tab slug and name (lowercase)
+	private $tab = 'faq';
+	// Set this to the page you want your tab to appear on (account, help and reports exist in Client Dash)
+	private $page = 'help';
+
+	// A URL/text field option
+	private $source_url = '_source_url';
+
+	// Set everything up
+	public function __construct() {
+		add_action( 'admin_notices', array( $this, 'notices' ) );
+		add_action( 'plugins_loaded', array( $this, 'content_block' ) );
+		add_action( 'admin_init', array( $this, 'register_settings' ) );
+		add_action( 'cd_settings_general_tab', array( $this, 'settings_display' ), 11 );
+		add_action( 'admin_enqueue_scripts', array( $this, 'register_styles' ) );
+	}
+
+	// Per CD 1.4, content blocks are the way to go
+	public function content_block() {
+		cd_content_block( $this->block_name, $this->page, $this->tab, array( $this, 'block_contents' ) );
+	}
+
+	public function register_styles() {
+		wp_register_style( $this->pre , plugin_dir_url(__FILE__).'style.css' );
+		$page = get_current_screen();
+		$tab = $_GET['tab'];
+
+		if ( $page->id != 'dashboard_page_cd_'.$this->page && $tab != $this->tab )
+			return;
+
+		wp_enqueue_style( $this->pre );
+	}
+
+	// Notices for if CD is not active (no need to change)
+	public function notices() {
+		if ( !is_plugin_active( 'client-dash/client-dash.php' ) ) { ?>
+		<div class="error">
+			<p><?php echo $this->plugin; ?> requires <a href="http://w.org/plugins/client-dash">Client Dash</a> version 1.4 or greater.
+			Please install and activate <b>Client Dash</b> to continue using.</p>
+		</div>
+		<?php
+		}
+	}
+
+	// Register settings
+	public function register_settings() {
+		register_setting( 'cd_options_general', $this->pre.$this->source_url, 'esc_url_raw' );
+	}
+
+	// Add settings to General tab
+	public function settings_display() {
+		$source_url = $this->pre.$this->source_url;
+		?>
+	<table class="form-table">
+		<tbody>
+			<tr valign="top">
+				<th scope="row"><h3><?php echo $this->plugin; ?> settings</th>
+			</tr>
+			<tr valign="top">
+				<th scope="row">
+					<label for="<?php echo $source_url; ?>">Source URL</label>
+				</th>
+				<td><input type="text" 
+					id="<?php echo $source_url; ?>" 
+					name="<?php echo $source_url; ?>" 
+					value="<?php echo get_option( $source_url ); ?>" />
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	<?php }
+
+	// Insert the tab contents
+	public function block_contents() {
+		$source_url = get_option( $this->pre.$this->source_url );
+		$result = wp_remote_get( add_query_arg( 'time', time(), $source_url ) );
+		if ( is_wp_error( $result ) OR empty( $result ) ) {
+		echo '<h2>Please enter a valid source URL in <a href="'.cd_get_settings_url().'">Settings</a></h2>';
+		} else {
+		$posts = json_decode( $result['body'] );
+			if ( $posts ) {
+				echo '<ul>';
+				foreach ( $posts as $value ) {
+					$content = apply_filters( 'the_content', $value->post_content );
+					?>
+					<li><h3 class="cd-click" onclick="cd_updown('cd-<?php echo $value->post_name; ?>');">
+							<?php echo $value->post_title; ?>
+						</h3>
+						<div id="cd-<?php echo $value->post_name; ?>" style="display: none;">
+							<?php echo $content; ?>
+						</div>
+					</li>
+				<?php }
+				echo '</ul>';
+			}
+		}
 	}
 }
-add_action('admin_notices', 'cdwph_notices');
-
-// Register settings
-function cdwph_register_settings() {
-	register_setting('cd_options_general', 'cdwph_url', 'esc_url_raw');
-}
-add_action('admin_init', 'cdwph_register_settings');
-
-// Add settings
-function cdwph_settings_display() { ?>
-<table class="form-table">
-	<tbody>
-		<tr valign="top">
-			<th scope="row"><h3>WP Help settings</th>
-		</tr>
-		<tr valign="top">
-			<th scope="row"><label for="cdwph_url">Source URL</label></th>
-			<td><input type="text" id="cdwph_url" name="cdwph_url" value="<?php echo get_option('cdwph_url'); ?>" /></td>
-		</tr>
-	</tbody>
-</table>
-<?php }
-add_action('cd_settings_general_tab', 'cdwph_settings_display', 11);
-
-// Add the FAQ tab
-function cdwph_add_tab( $tabs ) {
-	$tabs['help']['FAQ'] = 'faq';
-	return $tabs;
-}
-add_filter('cd_tabs', 'cdwph_add_tab');
-
-// Output the tab content
-function cdwph() {
-$source_url = get_option('cdwph_url');
-$result = wp_remote_get( add_query_arg( 'time', time(), $source_url ) );
-if (is_wp_error( $result ) OR empty($result)) {
-	echo '<h2>Please enter a valid source URL in <a href="'.cd_get_settings_url().'">Settings</a></h2>';
-} else {
-$posts = json_decode( $result['body'] );
-	if ($posts) {
-		echo '<ul>';
-		foreach ($posts as $value) {
-			$content = apply_filters('the_content', $value->post_content);
-			?>
-			<li><h3 class="cd-click" onclick="cd_updown('cd-<?php echo $value->post_name; ?>');">
-					<?php echo $value->post_title; ?>
-				</h3>
-				<div id="cd-<?php echo $value->post_name; ?>" style="display: none;">
-					<?php echo $content; ?>
-				</div>
-			</li>
-		<?php }
-		echo '</ul>';
-	}
-}
-}
-add_action('cd_help_faq_tab', 'cdwph');
-?>
+// Instantiate the class
+$cdwph = new CDWPHelp;
